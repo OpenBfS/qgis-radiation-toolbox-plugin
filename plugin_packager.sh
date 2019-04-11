96570bc85c55d8a4ddbb0e63905f0bbbbced9004
Martin Landa <landa.martin@gmail.com>
2019-04-11 13:03:55 +0200

    safecast packager draft finished, close #51

--- plugin_packager.sh
+++ plugin_packager.sh
@@ -9,18 +9,28 @@ rm -rf $DIR ; mkdir $DIR
 
 safecast_packager() {
     cp -r . $DIR
+    (cd $DIR/docs;make html)
+    rm -rf $DIR/.git $DIR/.gitignore
     rm -rf $DIR/__pycache__ $DIR/layer/__pycache__ $DIR/reader/__pycache__ $DIR/style/__pycache__
     rm $DIR/icons/radiation*
     rm $DIR/layer/ers* $DIR/layer/pei*
     mv $DIR/metadata_safecast.txt $DIR/metadata.txt
-    rm $DIR/plugin_packager.sh
+    rm $DIR/plugin_packager.sh $DIR/plugin_upload.py
     rm $DIR/reader/pei* $DIR/reader/ers*
-    rm -r $DIR/ers* $DIR/pei*
-    rm -r $DIR/tests $DIR/scripts
+    rm -r $DIR/style/ers* $DIR/style/pei*
+    rm -r $DIR/test $DIR/scripts
+
+    sed -i 's/PLUGIN_TYPE = PluginType.Dev/PLUGIN_TYPE = PluginType.Safecast/g' $DIR/plugin_type.py
+}
+
+zip_packager() {
+    cd /tmp
+    zip $TYPE.zip $TYPE -r
 }
 
 if [ "$TYPE" == 'safecast' ]; then
     safecast_packager
 fi
+zip_packager
 
 exit 0
