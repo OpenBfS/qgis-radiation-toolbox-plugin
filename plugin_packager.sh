731eaa9c4d16d5e643c1eb11e8205f41758d9fd4
Martin Landa <landa.martin@gmail.com>
2019-04-11 11:54:23 +0200

    update packager, see #51

--- plugin_packager.sh
+++ plugin_packager.sh
@@ -9,11 +9,14 @@ rm -rf $DIR ; mkdir $DIR
 
 safecast_packager() {
     cp -r . $DIR
+    rm -rf $DIR/__pycache__ $DIR/layer/__pycache__ $DIR/reader/__pycache__ $DIR/style/__pycache__
     rm $DIR/icons/radiation*
     rm $DIR/layer/ers* $DIR/layer/pei*
     mv $DIR/metadata_safecast.txt $DIR/metadata.txt
     rm $DIR/plugin_packager.sh
     rm $DIR/reader/pei* $DIR/reader/ers*
+    rm -r $DIR/ers* $DIR/pei*
+    rm -r $DIR/tests $DIR/scripts
 }
 
 if [ "$TYPE" == 'safecast' ]; then
