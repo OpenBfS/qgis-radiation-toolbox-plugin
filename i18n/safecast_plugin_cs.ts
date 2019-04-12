b2bdaf26d30ebb22c454e6c54642d14881d9bc65
Martin Landa <landa.martin@gmail.com>
2019-04-12 09:26:22 +0200

    update czech localization, see #28

--- i18n/safecast_plugin_cs.ts
+++ i18n/safecast_plugin_cs.ts
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0" language="cs_CZ" sourcelanguage="">
+<!DOCTYPE TS>
+<TS version="2.1" language="cs_CZ">
 <context>
     <name>Safecast</name>
     <message>
         <location filename="../safecast_plugin.py" line="197"/>
         <source>&amp;Safecast</source>
-        <translation type="unfinished"></translation>
+        <translation>&amp;Safecast</translation>
     </message>
     <message>
         <location filename="../safecast_plugin.py" line="171"/>
         <source>Safecast</source>
-        <translation type="unfinished"></translation>
+        <translation>Safecast</translation>
     </message>
 </context>
 <context>
@@ -18,84 +19,86 @@
     <message>
         <location filename="../safecast_dockwidget.py" line="130"/>
         <source>Style &apos;{}&apos; not found</source>
-        <translation type="unfinished"></translation>
+        <translation>Styl &apos;{}&apos; nenalezen</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="156"/>
         <source>Load Safecast LOG file</source>
-        <translation type="unfinished"></translation>
+        <translation>Načíst Safecast LOG soubor</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="241"/>
         <source>LOG file (*.LOG)</source>
-        <translation type="unfinished"></translation>
+        <translation>LOG soubor (*.LOG)</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="267"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>Chyba</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="185"/>
         <source>Failed to load input file &apos;{0}&apos;.
 
 Details: {1}</source>
-        <translation type="unfinished"></translation>
+        <translation>Selhalo načtení vstupního souboru &apos;{0}&apos;.
+
+Detaily: {1}</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="219"/>
         <source>Failed to apply style: {0}</source>
-        <translation type="unfinished"></translation>
+        <translation>Selhalo použití stylu: {0}</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="241"/>
         <source>Save layer as new LOG file</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit vrstvu jako nový LOG soubor</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="255"/>
         <source>Overwrite?</source>
-        <translation type="unfinished"></translation>
+        <translation>Přepsat?</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="255"/>
         <source>File {} already exists. Do you want to overwrite it?.</source>
-        <translation type="unfinished"></translation>
+        <translation>Soubor {} již existuje. Chcete ho přepsat?.</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="267"/>
         <source>Failed to save LOG file: {0}</source>
-        <translation type="unfinished"></translation>
+        <translation>Selhalo uložení LOG souboru: {0}</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="313"/>
         <source>Delete?</source>
-        <translation type="unfinished"></translation>
+        <translation>Smazat?</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="313"/>
         <source>Do you want to delete {} selected features? This operation cannot be reverted.</source>
-        <translation type="unfinished"></translation>
+        <translation>Chcete smazat {} vybraných bodů? Tato operace je nevratná.</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="344"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Info</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="328"/>
         <source>No features selected. Nothing to be deleled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Nic nevybráno. Není co smazat.</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="342"/>
         <source>No layer loaded or selected</source>
-        <translation type="unfinished"></translation>
+        <translation>Žádná vrstva není načtena nebo vybrána</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget.py" line="344"/>
         <source>No active layer available.</source>
-        <translation type="unfinished"></translation>
+        <translation>Žádná aktivní vrstva není k dispozici.</translation>
     </message>
 </context>
 <context>
@@ -103,82 +106,82 @@ Details: {1}</source>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="26"/>
         <source>Safecast</source>
-        <translation type="unfinished"></translation>
+        <translation>Safecast</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="43"/>
         <source>Load and Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>Načíst a Upravit</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="55"/>
         <source>Style:</source>
-        <translation type="unfinished"></translation>
+        <translation>Styl:</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="74"/>
         <source>Apply</source>
-        <translation type="unfinished"></translation>
+        <translation>Použít</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="107"/>
         <source>Import</source>
-        <translation type="unfinished"></translation>
+        <translation>Načíst</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="110"/>
         <source>Import LOG file as new layer</source>
-        <translation type="unfinished"></translation>
+        <translation>Načíst LOG soubor jako novou vrstvu</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="113"/>
         <source>Ctrl+I</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+I</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="122"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="125"/>
         <source>Save layer as new LOG file</source>
-        <translation type="unfinished"></translation>
+        <translation>Uložit vrstvu jako nový LOG soubor</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="128"/>
         <source>Ctrl+S</source>
-        <translation type="unfinished"></translation>
+        <translation>Ctrl+S</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="137"/>
         <source>Select</source>
-        <translation type="unfinished"></translation>
+        <translation>Vybrat</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="140"/>
         <source>Select features to cut</source>
-        <translation type="unfinished"></translation>
+        <translation>Vyberte body k odstranění</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="149"/>
         <source>Deselect</source>
-        <translation type="unfinished"></translation>
+        <translation>Zrušit výběr</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="152"/>
         <source>Deselect features</source>
-        <translation type="unfinished"></translation>
+        <translation>Zrušit výběr bodů</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="161"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>Smazat</translation>
     </message>
     <message>
         <location filename="../safecast_dockwidget_base.ui" line="164"/>
         <source>Delete selected features</source>
-        <translation type="unfinished"></translation>
+        <translation>Smazat vybrané body</translation>
     </message>
 </context>
 <context>
@@ -186,117 +189,117 @@ Details: {1}</source>
     <message>
         <location filename="../safecast_layer.py" line="86"/>
         <source>ADER microSv/h</source>
-        <translation type="unfinished"></translation>
+        <translation>ADER microSv/h</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="87"/>
         <source>Local time</source>
-        <translation type="unfinished"></translation>
+        <translation>Místní čas</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="88"/>
         <source>Device</source>
-        <translation type="unfinished"></translation>
+        <translation>Zařízení</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="89"/>
         <source>Device ID</source>
-        <translation type="unfinished"></translation>
+        <translation>ID zařízení</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="90"/>
         <source>Datetime</source>
-        <translation type="unfinished"></translation>
+        <translation>Datum a čas</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="91"/>
         <source>CPM</source>
-        <translation type="unfinished"></translation>
+        <translation>CPM</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="92"/>
         <source>Pulses 5sec</source>
-        <translation type="unfinished"></translation>
+        <translation>Impulzů za 5s</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="93"/>
         <source>Pulses total</source>
-        <translation type="unfinished"></translation>
+        <translation>Impulzů celkem</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="94"/>
         <source>Validity</source>
-        <translation type="unfinished"></translation>
+        <translation>Platnost</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="95"/>
         <source>Latitude (deg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Zem. šířka (°)</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="96"/>
         <source>Hemisphere</source>
-        <translation type="unfinished"></translation>
+        <translation>Polokoule</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="97"/>
         <source>Longitude (deg)</source>
-        <translation type="unfinished"></translation>
+        <translation>Zem. délka (°)</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="98"/>
         <source>East/West</source>
-        <translation type="unfinished"></translation>
+        <translation>Východ/Západ</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="99"/>
         <source>Altitude</source>
-        <translation type="unfinished"></translation>
+        <translation>Nadm. výška</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="100"/>
         <source>GPS Validity</source>
-        <translation type="unfinished"></translation>
+        <translation>Platnost GPS</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="101"/>
         <source>Sat</source>
-        <translation type="unfinished"></translation>
+        <translation>Sat</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="102"/>
         <source>HDOP</source>
-        <translation type="unfinished"></translation>
+        <translation>HDOP</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="103"/>
         <source>CheckSum</source>
-        <translation type="unfinished"></translation>
+        <translation>CheckSum</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="138"/>
         <source>Loading data...</source>
-        <translation type="unfinished"></translation>
+        <translation>Načítám data...</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="159"/>
         <source>Info</source>
-        <translation type="unfinished"></translation>
+        <translation>Info</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="159"/>
         <source>{} features loaded (in {:.2f} sec).</source>
-        <translation type="unfinished"></translation>
+        <translation>{} bodů načteno (během {:.2f} sec).</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="203"/>
         <source>Failed to read input data. Line: {}</source>
-        <translation type="unfinished"></translation>
+        <translation>Selhalo načtení vstupních dat. Řádek: {}</translation>
     </message>
     <message>
         <location filename="../safecast_layer.py" line="222"/>
         <source>unknown</source>
-        <translation type="unfinished"></translation>
+        <translation>neznámý</translation>
     </message>
 </context>
 </TS>
