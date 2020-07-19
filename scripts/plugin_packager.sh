cc338c3073755a6cd296100e3682879064b7b4f1
Martin Landa <landa.martin@gmail.com>
2020-07-19 15:31:51 +0200

    packager script: include pythonqwt, see #34

--- scripts/plugin_packager.sh
+++ scripts/plugin_packager.sh
@@ -9,22 +9,28 @@ rm -rf $DIR ; mkdir $DIR
 
 safecast_packager() {
     cp -r . $DIR
-    (cd $DIR/docs;make html)
-    mkdir $DIR/help
-    cp -r $DIR/docs/build/html/* $DIR/help
-    rm -rf $DIR/docs
+    (cd $DIR/help;make html)
+    mkdir $DIR/docs
+    cp -r $DIR/help/build/html/* $DIR/docs
+    rm -rf $DIR/help
     rm -rf $DIR/.git $DIR/.gitignore
     rm -rf $DIR/__pycache__ $DIR/layer/__pycache__ $DIR/reader/__pycache__ $DIR/style/__pycache__
     rm $DIR/icons/radiation*
     rm $DIR/layer/ers* $DIR/layer/pei*
     mv $DIR/metadata_safecast.txt $DIR/metadata.txt
-    rm $DIR/plugin_upload.py
     rm $DIR/reader/pei* $DIR/reader/ers*
     rm -r $DIR/style/ers* $DIR/style/pei*
-    rm -r $DIR/test $DIR/scripts
+    rm -r $DIR/scripts
 
     sed -i 's/PLUGIN_TYPE = PluginType.Dev/PLUGIN_TYPE = PluginType.Safecast/g' $DIR/plugin_type.py
     sed -i 's/safecast_icon_devel/safecast_icon_stable/g' $DIR/radiation_toolbox.py
+}
+
+pythonqwt_packager() {
+    TMPDIR=/tmp/pythonqwt
+    pip3 install PythonQwt -t $TMPDIR
+    cp -r $TMPDIR/qwt $DIR/
+    rm -rf $TMPDIR
 }
 
 zip_packager() {
@@ -36,6 +42,7 @@ zip_packager() {
 
 if [ "$TYPE" == 'safecast' ]; then
     safecast_packager
+    pythonqwt_packager
 fi
 zip_packager
 
